a322fa5c8bd07193fcf916b1fef821beca9dea21
Jarrett Cruger <jcruger@qrhook.com>
2013-03-22 18:12:36 -0400

    Frame of the browser object that will load the images on the fron tend from the static server

---
+++ browser/portfolio.js
@@ -0,0 +1,28 @@
+var hyperquest = require('hyperquest');
+
+module.exports = function () { return new Portfolio };
+
+function Portfolio () {
+    var self = this;
+    this.root = document.createElement('div');
+    this.root.className = 'list';
+
+    loadImages(function (images) {
+        self.images = images;
+        self.render();
+    });
+}
+
+Portfolio.prototype.appendTo = function (target) {
+    if (typeof target === 'string') target = document.querySelector(target);
+    target.appendChild(this.root);
+};
+
+Portfolio.prototype.render = function (src) {
+
+};
+
+function loadImages (cb) {
+
+}
+
